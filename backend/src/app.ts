b292f42bbe71b72a8fa1590539d082cf6e9d5521
powoftech <powoftech@gmail.com>
2023-10-25 15:01:24 +0700

    Setup MongoDB Atlas; Express error handling; Routes and Controllers

---
+++ backend/src/app.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import morgan from "morgan";
+import NotesRoute from "./routes/note";
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use("/api/notes", NotesRoute);
+
+app.use((req, res, next) => {
+	next(Error("Endpoint not found!"));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+	console.error(error);
+	let errorMessage = "An unknown error occurred!";
+	if (error instanceof Error) {
+		errorMessage = error.message;
+	}
+	res.status(500).json({ error: errorMessage });
+});
+
+export default app;
---
+++ backend/src/server.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+import env from "./util/validateEnv";
+import mongoose from "mongoose";
+
+const port = env.PORT;
+
+const mongo_connection_string = env.MONGO_CONNECTION_STRING;
+
+mongoose
+	.connect(mongo_connection_string)
+	.then(() => {
+		console.log("Mongoose connected!");
+
+		app.listen(port, () => {
+			console.log(`Server running on port: ${port}`);
+		});
+	})
+	.catch(() => {
+		console.error;
+	});
